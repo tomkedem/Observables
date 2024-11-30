a4e64a602fbbd5af32c8bd330ac72e456fddaeb6
Tomer Kedem <tompha1@hotmail.com>
2024-11-30 14:25:52 +0200

    Signals vs Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -9,8 +9,8 @@ import { interval, map } from 'rxjs';
 export class AppComponent implements OnInit{
   clickCount =  signal(0);
  private destroyRef = inject(DestroyRef)
-
- 
+ interval = signal(0);
+ doubleInterval = computed(() => this.interval() * 2)
  constructor() {
   effect(() => {
     console.log(`Clicked buttin ${this.clickCount()} times.`)
@@ -18,6 +18,10 @@ export class AppComponent implements OnInit{
  }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.interval.update(prevInternalNamber => prevInternalNamber +1);
+      
+    }, 1000)
     // const subscipton= interval(1000).pipe(
     //   map((val) => val * 2)
     // ).subscribe({
