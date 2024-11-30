6f39c39461bf57314d206373a7cb92f0f1849a19
Tomer Kedem <tompha1@hotmail.com>
2024-11-30 14:02:10 +0200

    Creating & Using an Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+ private destroyRef = inject(DestroyRef)
+
+  ngOnInit(): void {
+    const subscipton= interval(1000).subscribe({
+      next: (val) => console.log(val)            
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscipton.unsubscribe();
+    })
+  }
 
 }
